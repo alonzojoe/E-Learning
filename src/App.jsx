e1799d138a1d0a6b0a9930d5781ebeda1e9bc9b0
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-30 16:44:13 +0800

    feat: create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
-  return <main>App</main>;
+  return (
+    <main className="overflow-x-hidden bg-white text-dark">
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
