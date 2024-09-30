b77b46d21168854ccc881c197c309ad6df496e28
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-09-30 23:48:01 +0800

    feat: finish styling navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -34,9 +34,10 @@ const Navbar = () => {
     <nav>
       <div className="container py-10 flex justify-between items-center">
         {/* App Logo */}
-        <div className="font-bold text-2xl">
-          <h1>The E-Learning</h1>
+        <div>
+          <h1 className="font-bold text-2xl">The E-Learning</h1>
         </div>
+        {/* Menus */}
         <div className="hidden lg:block">
           <ul className="flex gap-4">
             {menus.map((menu) => (
@@ -45,14 +46,18 @@ const Navbar = () => {
                   href={menu.path}
                   className="inline-block py-2 px-3 hover:text-secondary relative group"
                 >
+                  <div className="h-2 w-2 absolute bg-secondary mt-2 left-1/2 -translate-x-1/2 top-1/2 bottom-0 hidden group-hover:block rounded"></div>
                   {menu.title}
                 </a>
               </li>
             ))}
+            <button className="primary-btn ">Sign In</button>
           </ul>
         </div>
-        {/* Menus */}
         {/* Mobile Menu */}
+        <div className="lg:hidden">
+          <IoMdMenu className="text-4xl" />
+        </div>
       </div>
     </nav>
   );
