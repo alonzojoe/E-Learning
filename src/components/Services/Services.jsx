8bb10d167460c6dd7bcb56517ce6e0f7b4bf65dd
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-10-02 16:18:25 +0800

    feat: finalized ui ux of services section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -58,10 +58,16 @@ const Services = () => {
         <h1 className="text-4xl font-bold text-left pb-10">
           Services we provide
         </h1>
-        <div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-8">
           {services.map((service) => (
-            <div key={service.id}>
-              <div>{service.icon}</div>
+            <div
+              className="bg-[#f4f4f4] rounded-2xl flex flex-col items-center gap-4 p-4 py-7 hover:shadow-2xl hover:bg-white hover:scale-110 duration-300"
+              key={service.id}
+            >
+              <div className="text-4xl mb-4">{service.icon}</div>
+              <h1 className="text-lg font-semibold text-center px-3">
+                {service.title}
+              </h1>
             </div>
           ))}
         </div>
