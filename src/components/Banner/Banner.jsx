e372fa073e5680b242db1f960b49932a277c0c5a
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-10-03 23:47:33 +0800

    feat: create ui ux for banner image section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,25 @@
-import React from "react";
 import BannerImg from "../../assets/images/banner.png";
 import { GrUserExpert } from "react-icons/gr";
 import { MdOutlineAccessTime } from "react-icons/md";
 import { FaBookReader } from "react-icons/fa";
+import { FadeUp } from "../Hero/Hero";
+import { motion } from "framer-motion";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>Banner Section</h1>
-    </div>
+    <section>
+      <div className="container py-14 md:py-24 grid grid-cols-1 md:grid-cols-2 gap-8 space-y-6 md:space-y-0">
+        {/* Banner Image */}
+        <div className="flex justify-center items-center">
+          <img
+            className="w-[350px] md:max-w-[450px] object-cover drop-shadow"
+            src={BannerImg}
+            alt="banner-image"
+          />
+        </div>
+        {/* Banner Text*/}
+      </div>
+    </section>
   );
 };
 
