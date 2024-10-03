42e01dff158db247a7fda047ae0966194a782b7d
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-10-03 23:37:23 +0800

    feat: create Banner component, import all required assets

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BannerImg from "../../assets/images/banner.png";
+import { GrUserExpert } from "react-icons/gr";
+import { MdOutlineAccessTime } from "react-icons/md";
+import { FaBookReader } from "react-icons/fa";
+
+const Banner = () => {
+  return (
+    <div>
+      <h1>Banner Section</h1>
+    </div>
+  );
+};
+
+export default Banner;
