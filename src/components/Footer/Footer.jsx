89210507b99a38176139cc5cd5757bfb21563aec
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-10-07 23:15:53 +0800

    feat: create ui uix for Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,93 @@ import { TbWorldWww } from "react-icons/tb";
 import { motion } from "framer-motion";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-28 bg-[#eceaea]">
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 md:gap-4">
+          {/* Section 1 */}
+          <div className="space-y-4 max-w-[300px]">
+            <h1 className="text-2xl font-bold">The E-Learning</h1>
+            <p className="text-dark2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+              natus itaque dolores reiciendis nihil ab nemo aliquid doloremque
+              delectus laboriosam ea, provident, voluptatibus autem nobis
+              quaerat quisquam. Magnam, possimus dolorum?
+            </p>
+          </div>
+          {/* Section 2 */}
+          <div className="grid grid-cols-2 gap-10">
+            <div className="space-y-4">
+              <h1 className="text-2xl font-bold">Courses</h1>
+              <div className="text-dark2">
+                <ul className="space-y-2 text-lg">
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Web Development
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Software Development
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Apps Development
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    E-Learning
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="space-y-4">
+              <h1 className="text-2xl font-bold">Links</h1>
+              <div className="text-dark2">
+                <ul className="space-y-2 text-lg">
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Home
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Service
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    About
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Contact
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* Section 3 */}
+          <div className="space-y-4 max-w-[300px]">
+            <h1 className="text-2xl font-bold">Get In Touch</h1>
+            <div className="flex items-center">
+              <input
+                type="email"
+                className="p-3 rounded-s-xl bg-white w-full py-4 focus:ring-0 focus:outline-none placeholder:text-dark2"
+                placeholder="Enter your email"
+              />
+              <button className="bg-primary text-white font-semibold py-4 px-6 hover:bg-secondary rounded-e-xl">
+                Go
+              </button>
+            </div>
+            <div className="flex space-x-6 py-3">
+              <a href="#">
+                <FaWhatsapp className="text-2xl cursor-pointer hover:text-primary hover:scale-105 duration-200" />
+              </a>
+              <a href="#">
+                <FaInstagram className="text-2xl cursor-pointer hover:text-primary hover:scale-105 duration-200" />
+              </a>
+              <a href="#">
+                <TbWorldWww className="text-2xl cursor-pointer hover:text-primary hover:scale-105 duration-200" />
+              </a>
+              <a href="#">
+                <FaYoutube className="text-2xl cursor-pointer hover:text-primary hover:scale-105 duration-200" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
